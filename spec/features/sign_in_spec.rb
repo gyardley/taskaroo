d057248a349d6ed51852fefe4ab531ba882d90ef
Emmy Yardley <emmy@yardley.ca>
2014-03-07 16:41:50 +0000

    Added test to check for 'Signed in' text after clicking link to 'Sign in to Twitter'.

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -8,5 +8,12 @@ describe "integration tests for sign in" do
     visit root_path
     expect(page).to have_content "Sign in to Twitter"
   end
+
+  it "should show 'Signed in' when 'Sign in to Twitter' link is clicked" do
+    visit root_path
+    click_link "Sign in to Twitter"
+    expect(page).to have_content "Signed in"
+    expect(page).to have_no_content "Sign in to Twitter"
+  end
   
 end
