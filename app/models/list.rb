26bfc47fec6bab6dd7a64319c641a7d58e9fe8bc
Emmy Yardley <emmy@yardley.ca>
2014-03-10 18:13:19 +0000

    Created validation tests for List and wrote code to make tests pass.

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,4 +1,6 @@
 class List < ActiveRecord::Base
   belongs_to :user
 
+  validates :name, presence: true
+  validates :user, presence: true
 end
--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -1,5 +1,34 @@
 require 'spec_helper'
 
 describe List do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  # validate that a list cannot be created without a name.
+    # try to create a list without a name
+    # assert that the list was not created
+
+  describe "validation tests" do
+    
+    before(:each) do
+      @new_list = List.new(name: "gazebo", user: User.new)
+    end
+
+    it "is invalid if it doesn't have a name" do
+
+      @new_list.name = nil
+      @new_list.should_not be_valid
+    end
+
+    it "is invalid if the name is blank" do
+
+      @new_list.name = ""
+      @new_list.should_not be_valid
+    end
+
+    it "is invalid if it's not associated with a user" do
+   
+      @new_list.user = nil
+      @new_list.should_not be_valid
+    end
+  end
+  
 end
