4216b297829df36f4a6dc21497f955a896a206ab
Emmy Yardley <emmy@yardley.ca>
2014-03-11 17:14:55 +0000

    Created tests and wrote code for Lists#index.

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,43 @@
 class ListsController < ApplicationController
+
+  def index
+    # @topics = Topic.all
+    # @topics = Topic.page(params[:page]).per(10)
+    # @lists = List.visible_to(current_user).page(params[:page]).per(10)
+
+    # redirect_to(root_path, :alert => "You need to be signed in to view ToDo Lists.") unless user_signed_in?
+     
+
+    if current_user
+      # @lists = List.where(user_id: current_user.id)
+      @lists = current_user.lists
+    else
+      redirect_to(root_path, :alert => "You need to be signed in to view ToDo Lists.")
+    end
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
 end
--- spec/controllers/lists_controller_spec.rb
+++ spec/controllers/lists_controller_spec.rb
@@ -2,4 +2,51 @@ require 'spec_helper'
 
 describe ListsController do
 
+  describe "#index" do
+
+    context "while signed out" do
+
+      it "redirects to welcome page" do
+        get :index
+        response.should redirect_to root_path
+        flash[:alert].should eql "You need to be signed in to view ToDo Lists."
+      end
+    end
+
+    context "while signed in" do
+
+      # API incompatibility between Devise and RSpec 2.14.1 doesn't let you use before(:all) here.
+      before(:each) do
+        @user = User.create
+        sign_in @user
+      end
+
+      it "returns a page" do
+        get :index
+        response.should be_success
+      end
+
+      it "contains at least one of the user's list" do
+        new_list = List.create(name: "gazebo", user: @user)
+
+        get :index
+        assigns["lists"].first.should eql new_list
+      end
+
+      it "contains multiple of the user's lists if present" do
+        new_list_1 = List.create(name: "gazebo", user: @user)
+        new_list_2 = List.create(name: "gazebo", user: @user)
+
+        get :index
+        assigns["lists"].count.should eql 2
+      end
+
+      it "doesn't show lists for other users" do
+        new_list = List.create(name: "gazebo", user: User.create)
+
+        get :index
+        assigns["lists"].count.should eql 0
+      end
+    end
+  end
 end
