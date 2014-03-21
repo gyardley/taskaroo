2c81c09a88f59a12b76883f4577840e41c7d1d46
Emmy Yardley <emmy@yardley.ca>
2014-03-21 21:24:20 +0000

    Added tests for both sign in links on homepage to /spec/features/sign_in_spec.rb.

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -8,14 +8,24 @@ describe "integration tests for sign in" do
     @user = users(:user_1)
   end
 
-  it "should show link to 'Sign in with Twitter' on the homepage" do
+  it "should show two links to 'Sign in with Twitter' on the homepage" do
     visit root_path
     expect(page).to have_content "Sign in with Twitter"
+    find(".pull-right li").should have_link "Sign in with Twitter"
+    find_link("")[:href].should eql user_omniauth_authorize_path(:twitter)
+    find("#twitter-button")[:alt].should eql "Sign in with Twitter"
   end
 
   it "should show 'Signed in as @nickname' when 'Sign in with Twitter' link is clicked" do
     visit root_path
     sign_in_with_twitter
+    expect(page).to have_content "Signed in as @#{@user.nickname}"
+    expect(page).to have_no_content "Sign in with Twitter"
+  end
+
+  it "should show 'Signed in as @nickname' when 'Sign in with Twitter' image is clicked" do
+    visit root_path
+    click_link ""
     expect(page).to have_content "Signed in as @#{@user.nickname}"
     expect(page).to have_no_content "Sign in with Twitter"
   end
