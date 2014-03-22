4d1d0c3992d04d18ad75620700c837f59d44587f
Emmy Yardley <emmy@yardley.ca>
2014-03-22 17:56:27 -0300

    Updated 'Signed in as @username' in nav bar.

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -3,18 +3,16 @@
 class CallbacksController < Devise::OmniauthCallbacksController
 
   def twitter
-
     # Retrieve existing or create a new user.
     @valid_user = User.retrieve_or_create(request.env['omniauth.auth'])
 
     if @valid_user # Check to see if this is a valid user.
-      flash[:notice] = "You successfully signed in with Twtiter." # Show success message.
+      flash[:notice] = "You successfully signed in with Twitter." # Show success message.
       sign_in_and_redirect @valid_user # Log the user in and redirect to success location.
     else
       flash[:error] = "You were not able to sign in with Twitter." # Show failure message.
       redirect_to root_path
     end
-
   end
-
+  
 end
